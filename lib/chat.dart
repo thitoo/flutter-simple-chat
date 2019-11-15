6b1ffb0e51677d3caa61410d1172a9158bfe2b4f
ThitOo <thitoo07@gmail.com>
2019-11-15 10:03:44 +0630

    add chat

---
+++ lib/chat.dart
@@ -0,0 +1,263 @@
+class Chat{
+  final String avatarUrl;
+  final String name;
+  final String datetime;
+  final String message;
+
+  Chat({this.avatarUrl, this.name, this.datetime, this.message});
+
+  static final List<Chat> data = [
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/34.jpg",
+      name: "Laurent",
+      datetime: "20:18",
+      message: "How about meeting tomorrow?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/49.jpg",
+      name: "Tracy",
+      datetime: "19:22",
+      message: "I love that idea, it's great!",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/women/77.jpg",
+      name: "Claire",
+      datetime: "14:34",
+      message: "I wasn't aware of that. Let me check",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/81.jpg",
+      name: "Joe",
+      datetime: "11:05",
+      message: "Flutter just release 1.0 officially. Should I go for it?",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/83.jpg",
+      name: "Mark",
+      datetime: "09:46",
+      message: "It totally makes sense to get some extra day-off.",
+    ),
+    Chat(
+      avatarUrl: "https://randomuser.me/api/portraits/men/85.jpg",
+      name: "Williams",
+      datetime: "08:15",
+      message: "It has been re-scheduled to next Saturday 7.30pm",
+    ),
+  ];
+}
